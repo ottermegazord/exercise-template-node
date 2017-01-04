ca3f948602e35912fba1730acedb0418651ee9f9
abelsan <abel@mit.edu>
2017-01-03 21:14:57 -0500

    template

--- starter_code/solution.js
+++ starter_code/solution.js
@@ -1,23 +1,14 @@
 var exercise = {};
 
 exercise.first = function(){
-    //-------------------
-    //---- Your Code ----
-    //-------------------    
     return 'Success: 1st function works!';    
 };
 
 exercise.second = function(greeting){
-    //-------------------
-    //---- Your Code ----
-    //-------------------
     return 'Success: 2nd function works!';    
 };
 
 exercise.third = function(user){
-    //-------------------
-    //---- Your Code ----
-    //------------------- 
     return 'Success: 3rd function works!';
 };
 
