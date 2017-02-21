f9c32e1f8c612aea2536ed58accac1c376be68a7
abelsan <abel@mit.edu>
2017-02-21 08:32:24 -0500

    should to expect

--- starter_code/test/test.js
+++ starter_code/test/test.js
@@ -1,21 +1,21 @@
-var should = require('chai').should();
+var expect = require('chai').expect;
 var ex = require('../solution.js');
 
 describe('Call 1st Function', function() {
 	it('Match Response Message', function() {
-	    ex.first().should.equal('Success: 1st function works!');
+	    expect(ex.first()).to.equal('Success: 1st function works!');
 	});
 });
 
 describe('Call 2nd Function', function() {
 	it('Match Response Message', function() {
-	    ex.second().should.equal('Success: 2nd function works!');
+	    expect(ex.second()).to.equal('Success: 2nd function works!');
 	});
 });
 
 describe('Call 3rd Function', function() {
 	it('Match Response Message', function() {
-	    ex.third().should.equal('Success: 3rd function works!');
+	    expect(ex.third()).to.equal('Success: 3rd function works!');
 	});
 });
 
